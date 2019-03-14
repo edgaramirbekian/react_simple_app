701b3fc629c53634fcb81011f02b326b0e064454
Edgar Amirbekian <amirbekianedgar@gmail.com>
2019-03-14 18:32:02 +0400

    add comment-add form and its validation highlighting

---
+++ src/components/comments/CommentAdd.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './styles/comment_add.css';
+
+export default class CommentAdd extends React.PureComponent {
+
+    state = {
+        name: '',
+        text: '',
+    }
+
+    limits = {
+        name: {max: 15, min: 5},
+        text: {max:100, min: 5}
+    }
+
+    render () {
+        return (
+            <form onSubmit = {null}>
+               <input
+                    placeholder = 'user'
+                    value = {this.state['name']} 
+                    onChange = {this.handleChange('name')} 
+                    className = {this.getClassName('name')()} 
+                />
+                <br />
+                <textarea
+                    placeholder = 'comment'
+                    value = {this.state['text']} 
+                    onChange = {this.handleChange('text')} 
+                    className = {this.getClassName('text')()}
+                />
+                <br />
+                <input type = 'submit' value = 'submit' />
+            </form>
+        )
+    }
+
+    handleChange = type => ev => {
+        const {value} = ev.target;
+        if (value.length >= this.limits[type].max) return
+        this.setState({
+            [type]: value
+        })
+    }
+
+    getClassName = type => ev => this.state[type].length < this.limits[type].min ? 'invalid' : 'valid'
+}
