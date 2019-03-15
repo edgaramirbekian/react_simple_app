94a469e11391512e36f937eafdab07986f042956
Edgar Amirbekian <amirbekianedgar@gmail.com>
2019-03-15 23:48:20 +0400

    add Filter component and move articles mock-data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import ArticleList from './components/articles/ArticleList';
 import CalendarDesk from './components/calendar/Calendar';
+import Filter from './components/filter/filter'
+import {articles} from '../src/data/mock';
+
 class App extends Component {
   render() {
     return (
       <div>
         <CalendarDesk />
-        <ArticleList />
+        <Filter articles = {articles}/>
+        <ArticleList articles = {articles}/>
       </div>
     );
   }
