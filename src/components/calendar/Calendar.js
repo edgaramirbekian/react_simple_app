f4cfc5f79f973f1f16616f23024940cac991780e
Edgar Amirbekian <amirbekianedgar@gmail.com>
2019-03-15 22:53:09 +0400

    add date select to calendar desk

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -2,11 +2,39 @@ import React from 'react';
 import DayPicker from 'react-day-picker';
 import "react-day-picker/lib/style.css";
 
-export default function CalendarDesk () {
-    return (
-        <div>
-            <h3>Date</h3>
-            <DayPicker selectedDays = {new Date()} />
-        </div>
-    )
+export default class CalendarDesk extends React.PureComponent {
+
+    constructor(props) {
+        super(props)
+        this.handleDayClick = this.handleDayClick.bind(this)
+        this.state = {
+            from: undefined,
+            to: new Date()
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <h3>Date</h3>
+                <DayPicker 
+                    onDayClick = {this.handleDayClick} 
+                    selectedDays = {this.state.from} 
+                />
+                {this.state.from 
+                    ? <p> You selected {this.state.from.toLocaleDateString()} </p>
+                    : <p> Please select a Date </p> }
+            </div>
+        )
+    }
+
+    handleDayClick (day, {selected}) {
+        selected
+        ? this.setState ({
+            from: undefined
+        })
+        : this.setState ({
+            from: day
+        })
+    }
 }
